55c424b7d0e1637eb9444d9e565515e777046279
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-12 01:08:13 -0700

    Upload file instead of response text

--- Service/app.js
+++ Service/app.js
@@ -89,7 +89,7 @@ app.get('/getDeviceStatus', function(req, res){
 });
 
 app.get('/getMCUInfo', function(req, res){
-	res.send("print(\"Hello World\")")
+	res.sendfile(__dirname +'/temp.lua')
 });
 
 // This endpoint will be called by EPS to add them self in the Home_Connect app.
