b0b9c1825c04b40e9c61ce0ccdc2a4b20b7bd36a
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-15 21:15:49 -0700

    Updating to create server with mqtt

--- mqtt.lua
+++ mqtt.lua
@@ -81,4 +81,32 @@ function run_main_prog()
 			print ( data )
 		end
 	end )
+	srv=net.createServer(net.TCP)
+	srv:listen(80,function(conn)
+		conn:on("receive", function(client,request)
+			local buf = "";
+			local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+			if(method == nil)then
+				_, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+			end
+			local _GET = {}
+			if (vars ~= nil)then
+			for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
+				_GET[k] = v
+			end
+	        end
+
+		local _on,_off = "",""
+		if(_GET.switch0 == "ON")then
+			buf.."{'Switch0':'ON'}"
+			gpio.write(switch0, gpio.HIGH);
+		elseif(_GET.switch0 == "OFF")then
+			buf.."{'Switch0':'OFF'}"
+			gpio.write(switch0, gpio.LOW);
+		end
+		client:send(buf);
+		client:close();
+		collectgarbage();
+		end)
+	end)
 end
