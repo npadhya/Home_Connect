6bef4e3c6fc8f18f5f8c2e382b3b6cdbf142db2a
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-15 23:03:23 -0700

    Disabling remove file for now

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 -- Wait for 2 secs and than run "main.lua" to avoid any Kernel panic
 tmr.alarm(0, 5000, 0, function()
 	print("Heap : "..node.heap())
-	fl = file.open("main.lua","r")
-	if ( fl ~= nil ) then
+	--fl = file.open("main.lua","r")
+	--if ( fl ~= nil ) then
 		node.compile("main.lua")
-	end
+	--end
 	print("Heap : "..node.heap())
-	file.remove("main.lua")
+	--file.remove("main.lua")
 	print("Heap : "..node.heap())
 	dofile("main.lc") 
 end )
