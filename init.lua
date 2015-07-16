8606163096650b064be6a0a2db027f57d4b50d97
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-15 22:30:48 -0700

    Check if file is present
    After first run the main.lua will be deleted, So next time just check if the source is present or not

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
 -- Wait for 2 secs and than run "main.lua" to avoid any Kernel panic
 tmr.alarm(0, 5000, 0, function()
 	print("Heap : "..node.heap())
-	node.compile("main.lua")
+	fl = file.open("main.lua","r")
+	if ( fl ~= nil ) then
+		node.compile("main.lua")
+	end
 	print("Heap : "..node.heap())
 	file.remove("main.lua")
 	print("Heap : "..node.heap())
