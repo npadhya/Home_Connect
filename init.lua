b12eb3a35a7f0473f48d55accf126e3a5f27b4b8
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-15 20:58:31 -0700

    try to same some memory
    Remove main.lua once compiled

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 -- Wait for 2 secs and than run "main.lua" to avoid any Kernel panic
 tmr.alarm(0, 5000, 0, function()
-	print("Heap memory available before compile : "..node.heap())
+	print("Heap : "..node.heap())
 	node.compile("main.lua")
-	print("Heap memory available after compile : "..node.heap())
+	print("Heap : "..node.heap())
+	file.remove("main.lua")
+	print("Heap : "..node.heap())
 	dofile("main.lc") 
 end )
