e93909e6407503d9033bcdc7ed9c3f0c4055d466
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-15 17:48:14 -0700

    Increasing time to load main.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Wait for 2 secs and than run "main.lua" to avoid any Kernel panic
-tmr.alarm(0, 2000, 0, function()
+tmr.alarm(0, 5000, 0, function()
 	print("Heap memory available before compile : "..node.heap())
 	node.compile("main.lua")
 	print("Heap memory available after compile : "..node.heap())
