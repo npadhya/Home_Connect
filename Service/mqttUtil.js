f78a853a5f0737cb7963c68e34a0f6924bf64466
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-17 07:14:05 -0700

    More function on msg recv

--- Service/mqttUtil.js
+++ Service/mqttUtil.js
@@ -1,10 +1,17 @@
 var client = mqttClient.connect('mqtt://192.168.42.1');
 
 client.subscribe("DSC");
-console.log("This is Working")
+client.subscribe("IMC");
+
+
 client.on("message", function(topic, payload) {
-  console.log(topic + " : " + payload);
-  client.end();
+  if(topic === 'IMC'){
+    console.log("IMC");
+    console.log(payload);
+  } else if ( topic === 'SDC'){
+    console.log("SDC");
+    console.log(payload);
+  }
 });
 
 client.publish("IMC", "hello world!");
