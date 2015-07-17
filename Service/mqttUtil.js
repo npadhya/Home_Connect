7fdcda3eba477490b3c92c141d9bf14024296b1c
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-17 07:00:52 -0700

    some basic plugin

--- Service/mqttUtil.js
+++ Service/mqttUtil.js
@@ -1,10 +1,10 @@
 var client = mqttClient.connect('192.168.42.1:1883');
 
-client.subscribe("IMC");
-
+client.subscribe("DSC");
+console.log("This is Working")
 client.on("message", function(topic, payload) {
   console.log(topic + " : " + payload);
   client.end();
 });
 
-client.publish("mqtt/demo", "hello world!");
+client.publish("IMC", "hello world!");
