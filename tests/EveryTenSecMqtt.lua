439faadec14318e1d223b0b8b2bc434789c1a2d6
Nikul Padhya <nikul.padhya@gmail.com>
2015-07-22 22:27:35 -0700

    MQTT pub every 10 sec

---
+++ tests/EveryTenSecMqtt.lua
@@ -0,0 +1,22 @@
+BROKER = "192.168.42.1"
+BRPORT = 1883
+CLIENTID = "ESP82661"
+
+pub_sem = 0
+current_topic  = 1
+
+m = mqtt.Client( CLIENTID, 120, "", "")
+m:connect( BROKER , BRPORT, 0, function(conn)
+  run_main_prog()
+end)
+
+function mqtt_pub()
+ current_topic = current_topic + 1
+	m:publish("DSC", current_topic,0,0, function(conn)
+	  print(current_topic)
+	end)
+end
+
+function run_main_prog()
+    tmr.alarm(5, 10000, 1, mqtt_pub )
+end
