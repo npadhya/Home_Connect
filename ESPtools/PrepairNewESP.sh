4dcfe923b6cfa4d60f57a90b43b2bec169f1e57a
Nikul Padhya <nikul.padhya@gmail.com>
2015-08-07 20:02:55 -0700

    Adding sleep 10 to remove the wire

--- ESPtools/PrepairNewESP.sh
+++ ESPtools/PrepairNewESP.sh
@@ -5,7 +5,9 @@ cd ~/Git/Home_Connect/ROMs
 sudo python ../ESPtools/esptool.py -p /dev/ttyAMA0 write_flash 0x00 nodemcu-custom-float.bin
 cd ..
 echo "Flash complete. Disconnect GPIO0 from GND and reboot the ESP"
+sleep 10
 cd ~/Git/Home_Connect/LUA_src
 sudo python ../ESPtools/nodemcu-uploader.py upload init.lua
+sleep 2
 sudo python ../ESPtools/nodemcu-uploader.py upload main.lua --compile
 
