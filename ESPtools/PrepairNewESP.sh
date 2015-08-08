7dbe1f8f07401c9dd49355c8dc084bad988e7988
Nikul Padhya <nikul.padhya@gmail.com>
2015-08-07 19:30:37 -0700

    Adding script to prepair a new ESP from scratch

---
+++ ESPtools/PrepairNewESP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Make sure to connect GPIO0 to GND and restart the ESP"
+cd ~/Git/Home_Connect/ROMs
+sudo python ../ESPtools/esptool.py -p /dev/ttyAMA0 write_flash 0x00 nodemcu-custom-float.bin
+cd ..
+echo "Flash complete. Disconnect GPIO0 from GND and reboot the ESP"
+cd ~/Git/Home_Connect/LUA_src
+sudo python ../ESPtools/nodemcu-uploader.py upload init.lua
+sudo python ../ESPtools/nodemcu-uploader.py upload main.lua --compile
+
