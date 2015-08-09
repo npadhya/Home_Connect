ffb63b6060d35dc860991527a14bf7cb1c9bea56
Nikul Padhya <nikul.padhya@gmail.com>
2015-08-08 19:34:33 -0700

    Changing file mode to Write instead of append

--- Service/mqttUtil.js
+++ Service/mqttUtil.js
@@ -4,30 +4,28 @@ var client = mqttClient.connect('mqtt://192.168.42.1');
 client.subscribe("DSC");
 client.subscribe("IMC");
 
-
 client.on("message", function(topic, payload) {
 	if(topic === 'IMC'){
 		console.log("IMC");
-		console.log(payload);
+		
 		var strPayload = payload.toString('utf8');
 		var deviceDetail = strPayload.split('-');
 		var deviceID = deviceDetail[0];
 		var deviceIP = deviceDetail[1];
 
 		var chipObject = {};
-		chipObject[RoomName] = "Default Room";
-		chipObject[ipAddress] = deviceIP;
+		chipObject["RoomName"] = "Default Room";
+		chipObject["ipAddress"] = deviceIP;
 
 		deviceList[deviceID] = chipObject;
 
-		fileSystem.appendFile('deviceList.json', JSON.stringify(deviceList), function (err) {
+		fileSystem.writeFile('deviceList.json', JSON.stringify(deviceList), function (err) {
 			if (err) throw err;
 			console.log('The DeviceList updated on disk!');
 		});
 	} else if ( topic === 'DSC'){
 		console.log("DSC");
-		console.log(payload);
-
+		
 		var strPayload = payload.toString('utf8');
 		var deviceStatusDetail = strPayload.split('-');
 
@@ -36,15 +34,15 @@ client.on("message", function(topic, payload) {
 		var newStatus = deviceStatusDetail[2];
 
 		var gpioDetail = {};
-		gpioDetail[DeviceName] = "Default Light";
-		gpioDetail[status] = newStatus;
+		gpioDetail["DeviceName"] = "Default Light";
+		gpioDetail["status"] = newStatus;
 
 		var devices = {};
 		devices[gpioPin] = gpioDetail;
 
 		deviceStatus[deviceIP] = devices;
 
-		fileSystem.appendFile('deviceStatus.json', JSON.stringify(deviceStatus), function (err) {
+		fileSystem.writeFile('deviceStatus.json', JSON.stringify(deviceStatus), function (err) {
 			if (err) throw err;
 			console.log('The DeviceList updated on disk!');
 		});
