18e43b3e92c146f02746803b6de709e3b802ccec
Nikunj Patel <direct2nnp@gmail.com>
2015-08-09 22:20:05 -0700

    solving callback function issue in lua

--- Service/temp.lua
+++ Service/temp.lua
@@ -1,34 +1,19 @@
 --START
-topics = {"RUT", "REBOOT"}
 pub_sem = 0
-current_topic  = 1
-topicsub_delay = 50
 swtch = { 
 	   switch1 = { interrupt = 3, output = 4},
 	   switch2 = { interrupt = 5, output = 6},
 	   switch3 = { interrupt = 7, output = 8},
 	   switch4 = { interrupt = 1, output = 2}
 		}
-for i in pairs(swtch) do
-    print(swtch[i].interrupt .. ' '.. swtch[i].output)
-    gpio.mode(swtch[i].interrupt, gpio.INT)
-	gpio.mode(swtch[i].output, gpio.OUTPUT)
-end
 m = mqtt.Client( "ESP"..node.chipid(), 120, "", "")
 m:connect( "192.168.42.1", 1883, 0, function(conn)
-	mqtt_sub()
+	m:subscribe("RUT", 0, function(conn)
+	end)
+	m:subscribe("REBOOT", 0, function(conn)
+	end)
 end)
-function mqtt_sub()
-	if table.getn(topics) < current_topic then
-		run_main_prog()
-	else
-		m:subscribe(topics[current_topic] , 0, function(conn)
-		end)
-		current_topic = current_topic + 1  -- Goto next topic
-		tmr.alarm(5, topicsub_delay, 0, mqtt_sub )
-	end
-end
-function publish_device_status_change(pin, newStatus)
+function publish_device_status_change(pin, newStatus) 
 	if pub_sem == 0 then
 		pub_sem = 1
 		m:publish("DSC", node.chipid().."-"..pin.."-"..newStatus,0,0, function(conn)
@@ -36,26 +21,14 @@ function publish_device_status_change(pin, newStatus)
 		end)
 	end
 end
-function switch1Callback(level)
-	publish_device_status_change(swtch['switch1'].output,level)
-	gpio.write(swtch['switch1'].output,level)
+for i in pairs(swtch) do
+    print(swtch[i].interrupt .. ' '.. swtch[i].output)
+    gpio.mode(swtch[i].interrupt, gpio.INT)
+	gpio.mode(swtch[i].output, gpio.OUTPUT)
+	gpio.trig(swtch[i].interrupt,"both",function(level)
+		publish_device_status_change(swtch[i].output,level)
+		gpio.write(swtch[i].output,level))
 end
-function switch2Callback(level)
-	publish_device_status_change(swtch['switch2'].output,level)
-	gpio.write(swtch['switch2'].output,level)
-end
-function switch3Callback(level)
-	publish_device_status_change(swtch['switch3'].output,level)
-	gpio.write(swtch['switch3'].output,level)
-end
-function switch4Callback(level)
-	publish_device_status_change(swtch['switch4'].output,level)
-	gpio.write(swtch['switch4'].output,level)
-end
-gpio.trig(swtch['switch1'].interrupt,"both",switch1Callback)
-gpio.trig(swtch['switch2'].interrupt,"both",switch2Callback)
-gpio.trig(swtch['switch3'].interrupt,"both",switch3Callback)
-gpio.trig(swtch['switch4'].interrupt,"both",switch4Callback)
 function run_main_prog()
 	m:publish("IMC", node.chipid()..'-'..wifi.sta.getip(), 0, 0 , function() end)
 	m:on("message", function(conn, topic, data)
